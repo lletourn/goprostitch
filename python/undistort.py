2d523a8f3f20b5bcf983a9db60d2e7cdc7a954df
Louis Letourneau <lletourn49@gmail.com>
2023-09-06 09:18:54 -0400

    Generalized undistort

--- python/undistort.py
+++ python/undistort.py
@@ -1,19 +1,28 @@
 import cv2  # type: ignore
+import json
 import numpy
 import sys
 
-K = numpy.array([[1768.8181032986872, 0, 1.9237091208494583e+03], [0, 1.7679751216421994e+03, 1.0957257960681286e+03], [0, 0, 1]], dtype=numpy.float64)
-distcoeffs = numpy.array([-2.3918513751023274e-01, 6.9352326700516775e-02, -5.8377437741893799e-05, 1.9806067043683098e-04,-1.0478418302033846e-02], dtype=numpy.float64)
+calibration_filename = sys.argv[1]
+input_img_filename = sys.argv[2]
+output_img_filename = sys.argv[3]
 
-img = cv2.imread(sys.argv[1])
+with open(calibration_filename, "r") as f:
+    data = json.load(f)
+    K = numpy.array(data["K"], dtype=numpy.float64)
+    distcoeffs = numpy.array(data["D"], dtype=numpy.float64)
+
+img = cv2.imread(input_img_filename)
 h, w = img.shape[:2]
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(K, distcoeffs, (w, h), 0, (w, h))
 mapx, mapy = cv2.initUndistortRectifyMap(K, distcoeffs, None, newcameramtx, (w, h), 5)
 dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
 
 print(dst.shape)
+cv2.namedWindow("Corrected", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Orig", cv2.WINDOW_NORMAL)
 cv2.imshow("Corrected", dst)
 cv2.imshow("Orig", img)
 cv2.waitKey(0)
 
-cv2.imwrite(sys.argv[2], dst)
+cv2.imwrite(output_img_filename, dst)
